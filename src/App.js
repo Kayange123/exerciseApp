06bc309cc5624fac17ec6788277d90979339c0f6
Ayubu Kayange <kayangejr3@gmail.com>
2023-02-14 18:40:42 +0300

    Folders created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./containers/Home";
+import ExerciseDetails from "./containers/ExerciseDetails";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Box width={400} sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
